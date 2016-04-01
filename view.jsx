535e6a30cef0b9f0fc202f6b83a30e7fb091293c
Ben White <bewhite@paypal.com>
2016-04-01 12:21:06 -0500

    introduce componentDidMount and custom function

--- view.jsx
+++ view.jsx
@@ -11,6 +11,26 @@ var Greeting = function Greeting(props) {
 				name: 'Ben'
 			}
 		},
+		changeName: function () {
+			var self = this,
+				names = ['Kevin', 'Satish', 'Justin', 'Ben'],
+				index = 0;
+			setInterval(function () {
+				var currentName;
+				if (typeof names[index] !== 'undefined') {
+					currentName = names[index];
+					index++;
+				} else {
+					index = 0;
+					currentName = names[0];
+					index++;
+				}
+				self.setState({ name: currentName });
+			}, 1000);
+		},
+		componentDidMount: function () {
+			this.changeName();
+		},
 		render: function render() {
 			return (
 				<Greeting name={ this.state.name } />
