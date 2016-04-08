4d834c24e8db5bd0a0ab2b6255537222608fce27
Ben White <bewhite@paypal.com>
2016-04-08 14:45:00 -0500

    add checkbox/todo completion to app

--- view.jsx
+++ view.jsx
@@ -1,12 +1,19 @@
 var Todo = function Todo(props) {
+		var isCompleted = props.todo.isCompleted ? 'completed' : '',
+			todoId = props.todo.id;
 		return (
-			<li key={props.key}>{props.todo}</li>
+			<li>
+				<label htmlFor={todoId}>
+					<input type="checkbox" id={todoId} name="completed" onChange={props.completeTodo}/>
+					<span className={isCompleted}>{props.todo.content}</span>
+				</label>
+			</li>
 		);
 	},
 	TodoList = function TodoList(props) {
 		return (
 			<ul>
-				{ props.todos.map(function (todo, idx) { return <Todo todo={todo} key={'todo-' + idx } />; }) }
+				{ props.todos.map(function (todo, idx) { return <Todo todo={todo} completeTodo={props.completeTodo} key={'todo-' + idx } />; }) }
 			</ul>
 		);
 	},
@@ -27,20 +34,32 @@ var Todo = function Todo(props) {
 		},
 		addTodo: function (event) {
 			var nameInput = event.currentTarget.name;
-			
+
 			event.preventDefault();
-			
+
 			this.setState({
-				todos: this.state.todos.concat([nameInput.value])
+				todos: this.state.todos.concat([{ content: nameInput.value, isCompleted: false, id: this.state.todos.length }])
 			});
-			
+
 			nameInput.value = '';
+		},
+		completeTodo: function (event) {
+			var todoId = event.target.id,
+				checked = event.target.checked,
+				todos = this.state.todos;
+
+			todoId = todoId && parseInt(todoId, 10);
+			todos[todoId].isCompleted = checked;
+
+			this.setState({
+				todos: todos
+			});
 		},
 		render: function render() {
 			return (
 				<div>
 					<TodoForm inputValue={ this.state.inputValue } addTodo={ this.addTodo } />
-					<TodoList todos={ this.state.todos } />
+					<TodoList todos={ this.state.todos } completeTodo={this.completeTodo} />
 				</div>
 			);
 		}
